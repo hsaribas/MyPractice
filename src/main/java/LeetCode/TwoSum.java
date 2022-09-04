80fc9dcc3c65775cafd2b7092398ba8a5ebddecb
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-09-04 18:39:44 +0300

    TwoSum

---
+++ src/main/java/LeetCode/TwoSum.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {2, 7, 11, 15};
+        int point = 9;
+
+        System.out.println(Arrays.toString(twoSum(numbers, point)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] arr = new int[2];
+
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+    }
+}
