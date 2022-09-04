13e7b46d234541537788a5c85a296895d85cc994
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-09-04 20:59:00 +0300

    Palindrom_Number

---
+++ src/main/java/LeetCode/Palindrome_Number.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class Palindrome_Number {
+
+    public static void main(String[] args) {
+
+        int x = 121;
+        System.out.println(isPalindrome(x));
+
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        String str = String.valueOf(x);
+        String newStr = "";
+
+        for(int i=str.length()-1; i>-1; i--){
+            newStr += str.charAt(i);
+        }
+        if(str.equals(newStr)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+}
