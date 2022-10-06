7179c588a8aee1d901012e065b55bf13bc0d50c0
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-10-06 22:43:19 +0300

    9th commit

---
+++ src/main/java/LeetCode/LastWordsLength.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+public class LastWordsLength {
+
+    public static void main(String[] args) {
+
+        String word = "Hello World";
+        System.out.println(lengthOfLastWord(word));
+
+    }
+
+    public static int lengthOfLastWord(String s) {
+
+        String[] arr = s.trim().split(" ");
+        String str = arr[arr.length-1];
+        int len = str.length();
+        return len;
+    }
+}
---
+++ src/main/java/LeetCode/Sqrt.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class Sqrt {
+
+    public static void main(String[] args) {
+
+        int a = 8;
+        System.out.println(mySqrt(a));
+
+    }
+
+    public static int mySqrt(int x) {
+
+        if(x==0 || x==1) return x;
+        return (int)Math.sqrt(x);
+    }
+}
