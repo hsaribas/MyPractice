99613675f207b2994c47b27eb7e27580195ac51e
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-12-27 21:15:35 +0300

    commit-14

---
+++ src/main/java/Practice/NearestFibonacci.java
@@ -0,0 +1,29 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class NearestFibonacci {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        if(num==0){
+            System.out.println(0);
+        }
+
+        int first = 0, second = 1;
+        int third = first + second;
+
+        while (third < num) {
+            first = second;
+            second = third;
+            third = first + second;
+        }
+
+        int dis = num - second;
+        int lastFibo = num - dis;
+        System.out.println(lastFibo);
+    }
+}
