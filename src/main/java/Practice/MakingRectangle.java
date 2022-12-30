f728932a1044a4edad288cc453ed4eb016cadb99
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-12-30 23:25:46 +0300

    commit-15

---
+++ src/main/java/Practice/MakingRectangle.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class MakingRectangle {
+
+    public static void main(String[] args) {
+
+        int height = 5;
+        int width = 7;
+        char ch = '$';
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/Practice/PassOrFail.java
@@ -0,0 +1,30 @@
+package Practice;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class PassOrFail {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int ex = scan.nextInt();
+        int score = 0;
+        String p = "PASS";
+        String f = "FAIL";
+
+        for(int i=1; i<=ex; i++){
+            String attempt = scan.next();
+            if(Objects.equals(attempt, p) && i<ex){
+                score += 6;
+            }else if(Objects.equals(attempt, p) && i==ex){
+                score +=10;
+            }else if(Objects.equals(attempt, f) && score>=6){
+                score -= 6;
+            }else if(Objects.equals(attempt, f) && score==0){
+                score += 0;
+            }
+        }
+        System.out.println(score);
+    }
+}
