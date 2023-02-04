6072faba7a132997197d9ca8c35bfaf5e97a96d9
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-02-04 20:55:51 +0300

    commit-24

---
+++ src/main/java/LeetCode/GenerateParentheses.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public static void main(String[] args) {
+
+        int num = 4;
+        System.out.println(generateParenthesis(num));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void backtrack(List<String> result, String curr, int open, int close, int max) {
+
+        if (curr.length() == max * 2) {
+            result.add(curr);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(result, curr + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(result, curr + ")", open, close + 1, max);
+        }
+    }
+}
