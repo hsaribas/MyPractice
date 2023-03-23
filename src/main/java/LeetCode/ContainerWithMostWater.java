4df9eae932bf28f39afc9b848db236ba07df07c7
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-03-23 23:48:42 +0300

    Commit-29

---
+++ src/main/java/LeetCode/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+
+    public static int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
