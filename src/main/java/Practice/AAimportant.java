f2320ed262532f948d3b5aedd18fcd398a3235af
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-04-02 16:28:37 +0300

    commit-30

---
+++ src/main/java/Practice/AAimportant.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Random;
+
+public class AAimportant {
+
+    int a = Integer.MAX_VALUE;
+    int b = Integer.MIN_VALUE;
+    int c = Math.abs(a);
+    int d = Math.round(b);
+    int e = Math.max(a, b);
+    int f = Math.min(a, b);
+
+    String x = "19";
+    String y = String.valueOf(a);
+
+    int g = Integer.parseInt(x);
+    int h = (int) (Math.random() + 100);
+
+    Random r = new Random();
+}
